0fd487f084a72eb852f7864f9fea1b27faae7e7e
IvanCarlosJr <ivancarlosduartejunior@gmail.com>
2023-01-04 16:43:25 -0300

    React app

---
+++ discover/reactapp/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h1>Lista de Presença.</h1>
+      <input type="text" placeholder="Digite o nome..." />
+      <button type="button">Adicionar</button>
+    </div>
+  )
+}
+
+export default Home
